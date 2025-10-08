4d8ddffed3adfef1f312ba283fe0ca99f28105d2
David Biagiola <marcelobiagiola01@gmail.com>
2025-10-08 09:54:48 -0300

    add the add, mult inv_addition examples with its tests

--- ecc/src/lib.rs
+++ ecc/src/lib.rs
@@ -31,23 +31,123 @@ impl EllipticCurve {
 struct FiniteField {}
 
 impl FiniteField {
-    fn add(c: &BigUint, d: &BigUint) -> BigUint {
+    fn add(c: &BigUint, d: &BigUint, p: &BigUint) -> BigUint {
         // c + d = r mod p
-        todo!()
+        let r = c + d;
+
+        // modpow elevetes to a power and then applies to a module
+        // r.modpow(exponent, modulus)
+        // note: exponent of 1 is always the same number so he does the modulo
+        // operation only.
+        r.modpow(&BigUint::from(1u32), p)
     }
 
     fn mult(c: &BigUint, d: &BigUint, p: &BigUint) -> BigUint {
         // c * d = r mod p
-        todo!()
+        let r = c * d;
+
+        r.modpow(&BigUint::from(1u32), p)
     }
 
-    fn inv_add(c: &BigUint, p: &BigUint) -> BigUint {
+    fn inv_addition(c: &BigUint, p: &BigUint) -> BigUint {
         // -c mod p
-        todo!()
+        // c is less than p because we're operating inside the group
+        // and shouldn't allow a number to be bigger than the module operation.
+        assert!(c < p, "c >= p");
+        // format!("number: {} is bigger or equal than p: {}", c, p);
+        p - c
     }
 
     fn inv_multiplication(c: &BigUint, p: &BigUint) -> BigUint {
         // c^(-1) mod p
         todo!()
     }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_add_1() {
+        let c = BigUint::from(4u32);
+        let d = BigUint::from(10u32);
+        let p = BigUint::from(11u32);
+
+        // 4 * 10 mod 11 = 3
+        let r = FiniteField::add(&c, &d, &p);
+
+        assert_eq!(r, BigUint::from(3u32));
+    }
+
+    #[test]
+    fn test_add_2() {
+        let c = BigUint::from(4u32);
+        let d = BigUint::from(10u32);
+        let p = BigUint::from(31u32);
+
+        // 4 * 10 mod 11 = 3
+        let r = FiniteField::add(&c, &d, &p);
+
+        assert_eq!(r, BigUint::from(14u32));
+    }
+
+    #[test]
+    fn test_mult_1() {
+        let c = BigUint::from(4u32);
+        let d = BigUint::from(10u32);
+        let p = BigUint::from(11u32);
+
+        // 4 * 10 mod 11 = 3
+        let r = FiniteField::mult(&c, &d, &p);
+
+        assert_eq!(r, BigUint::from(7u32));
+    }
+
+    #[test]
+    fn test_mult_2() {
+        let c = BigUint::from(4u32);
+        let d = BigUint::from(10u32);
+        let p = BigUint::from(51u32);
+
+        // 4 * 10 mod 11 = 3
+        let r = FiniteField::mult(&c, &d, &p);
+
+        assert_eq!(r, BigUint::from(40u32));
+    }
+
+    #[test]
+    fn test_inv_addition_1() {
+        let c = BigUint::from(4u32);
+        let p = BigUint::from(51u32);
+
+        // p - c
+        let r = FiniteField::inv_addition(&c, &p);
+
+        assert_eq!(r, BigUint::from(47u32));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_inv_addition_2() {
+        let c = BigUint::from(52u32);
+        let p = BigUint::from(51u32);
+
+        // p - c
+        FiniteField::inv_addition(&c, &p);
+    }
+
+    #[test]
+    fn test_inv_addition_identity() {
+        let c = BigUint::from(4u32);
+        let p = BigUint::from(51u32);
+
+        // p - c
+        let c_inv = FiniteField::inv_addition(&c, &p);
+
+        assert_eq!(c_inv, BigUint::from(47u32));
+        assert_eq!(FiniteField::add(&c, &c_inv, &p), BigUint::from(0u32))
+        // 0 is the identity element in an addition
+    }
+
 }
